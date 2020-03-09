e122ca185a51ab704b206f85ddb00f14572b2170
James Berry <git@jamesberry.uk>
2020-03-09 09:12:10 +0000

    Had problems committing LFS files, made new repo to fix it

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# SubSub
-Comp 140 project
+# comp140-Project
+Base repositry for the COMP140 - Creative Computing Project
---
+++ Unity Project/SubSub/Assets/Scripts/Valve.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Valve : MonoBehaviour
+{
+    [SerializeField]
+    private float currentTurnAmount;
+    [SerializeField]
+    private float turnSensitivity;
+    private float turnLenciency;
+
+    private float desiredAngle;
+    private bool shouldSink;
+
+    public HealthControl healthLeft;
+
+    [Header ("Keyboard Debug")]
+    [SerializeField]
+    private KeyCode leftTurn;
+    [SerializeField]
+    private KeyCode rightTurn;
+
+    private void Start()
+    {
+        currentTurnAmount = 0;
+        StartCoroutine(AngleDecider());
+    }
+
+    private IEnumerator AngleDecider()
+    {
+        if (Random.value >= .75f)
+        {
+            desiredAngle = Random.Range(0, 360);
+        }
+        print(desiredAngle);
+        Sink();
+        yield return new WaitForSeconds(1);
+        StartCoroutine(AngleDecider());
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(leftTurn) && !Input.GetKeyDown(rightTurn))
+        {
+            currentTurnAmount -= turnSensitivity;
+        }
+        else if(Input.GetKey(rightTurn) && !Input.GetKeyDown(leftTurn))
+        {
+            currentTurnAmount += turnSensitivity;
+        }
+
+        if (currentTurnAmount >= desiredAngle - turnLenciency && currentTurnAmount <= desiredAngle + turnLenciency)
+        {
+            shouldSink = false;
+        }
+        else
+        {
+            shouldSink = true;
+        }
+    }
+
+    private void Sink()
+    {
+        if (shouldSink)
+        {
+            shouldSink = false;
+        }
+    }
+}
